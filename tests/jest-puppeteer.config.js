048263b25b80730757eb1e96f972c2bd65e86dc2
sandyr <sandyr@ebi.ac.uk>
2022-02-11 15:47:43 +0000

    more debug

--- tests/jest-puppeteer.config.js
+++ tests/jest-puppeteer.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   launch: {
-    headless: true
+    headless: true,
+    dumpio: true
+
   },
+  browser: 'chromium',
+  browserContext: 'default',
   server: {
     command: "cd ../shiny-proxy; java -jar shinyproxy-2.6.0.jar",
     port: 8080,
-    launchTimeout: 180000
+    launchTimeout: 180000,
+    debug: true
   },
-  devtools: true
 };
