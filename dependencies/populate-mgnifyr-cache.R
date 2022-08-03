4ad4e0623b4fc709c340a53058cf45bb116b651f
Ales-ibt <ales@ebi.ac.uk>
2022-08-03 13:51:48 +0100

    Second round for comparative metagenomics 1

--- dependencies/populate-mgnifyr-cache.R
+++ dependencies/populate-mgnifyr-cache.R
@@ -1,12 +1,16 @@
-# Pre-populate MGonifyR Cache with default examples, to save expensive API calls
-
+# Pre-populate MGnifyR Cache with default examples, to save expensive API calls
 library(MGnifyR)
+library(stringr)
 
-# For the "Fetch Analyses metadata" notebook
+
 mg <- mgnify_client(usecache = T, cache_dir = '/home/jovyan/.mgnify_cache')
-analyses_accessions <- mgnify_analyses_from_studies(mg, 'MGYS00005116')
-analyses_metadata_df <- mgnify_get_analyses_metadata(mg, analyses_accessions)
 
-# For the "Comparative Metagenomics" notebook
+# For the "Comparative Metagenomics 1" notebook
 tara_all = mgnify_analyses_from_studies(mg, 'MGYS00002008')
 metadata = mgnify_get_analyses_metadata(mg, tara_all)
+v5_metadata = metadata[which(metadata$'analysis_pipeline-version'=='5.0'), ]
+sub1 = v5_metadata[str_detect(v5_metadata$'sample_environment-feature', "ENVO:00002042"), ]
+sub2 = v5_metadata[str_detect(v5_metadata$'sample_environment-feature', "ENVO:00000213"), ]
+filtered_samples = rbind(sub1,sub2)
+ps = mgnify_get_analyses_phyloseq(mg, filtered_samples$'analysis_accession')
+
